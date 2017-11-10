af1593478ca7097d3451295632fdaa376beb480d
Yuriy Liskov <firsthash@gmail.com>
2017-11-10 17:38:37 +0200

    exo: ui update

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/widgets/LayoutToggleButton.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/widgets/LayoutToggleButton.java
@@ -4,16 +4,21 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 import com.liskovsoft.exoplayeractivity.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LayoutToggleButton extends TextToggleButton {
     private static final int LAYOUT_CHECKED = 11;
-    private int mOtherToggleId;
+    private static final int LAYOUT_TOGGLE_BUTTON = 12;
     private int mLayoutId1;
     private int mLayoutId2;
     private View mLayout1;
     private View mLayout2;
-    private View mOtherToggle;
+    private List<View> mButtons;
 
     public LayoutToggleButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -34,7 +39,6 @@ public class LayoutToggleButton extends TextToggleButton {
         try {
             mLayoutId1 = a.getResourceId(R.styleable.LayoutToggleButton_layout1, 0);
             mLayoutId2 = a.getResourceId(R.styleable.LayoutToggleButton_layout2, 0);
-            mOtherToggleId = a.getResourceId(R.styleable.LayoutToggleButton_other_toggle, 0);
         } finally {
             a.recycle();
         }
@@ -47,7 +51,34 @@ public class LayoutToggleButton extends TextToggleButton {
     }
 
     private void init() {
+        setTag(LAYOUT_TOGGLE_BUTTON, true);
         //findLayouts();
+    }
+
+    /**
+     * Get all the views which matches the given Tag recursively
+     * @param root parent view. for e.g. Layouts
+     * @param tagId tag id to look for
+     * @return List of views
+     */
+    public static List<View> findAllViewsWithTagId(ViewGroup root, int tagId){
+        List<View> allViews = new ArrayList<>();
+
+        final int childCount = root.getChildCount();
+        for(int i=0; i<childCount; i++){
+            final View childView = root.getChildAt(i);
+
+            if(childView instanceof ViewGroup){
+                allViews.addAll(findAllViewsWithTagId((ViewGroup)childView, tagId));
+            }
+            else{
+                final Object tagView = childView.getTag(tagId);
+                if(tagView != null)
+                    allViews.add(childView);
+            }
+        }
+
+        return allViews;
     }
 
     @Override
@@ -57,7 +88,8 @@ public class LayoutToggleButton extends TextToggleButton {
         View rootView = getRootView();
         mLayout1 = rootView.findViewById(mLayoutId1);
         mLayout2 = rootView.findViewById(mLayoutId2);
-        mOtherToggle = rootView.findViewById(mOtherToggleId);
+
+        mButtons = findAllViewsWithTagId((ViewGroup) rootView, LAYOUT_TOGGLE_BUTTON);
     }
 
     @Override
@@ -85,35 +117,57 @@ public class LayoutToggleButton extends TextToggleButton {
     }
 
     private void disableOtherToggles() {
-        if (mOtherToggle == null) {
-            return;
+        for (View button : mButtons) {
+            if (button != this)
+                ((LayoutToggleButton) button).disable();
         }
-
-        mOtherToggle.setFocusable(false);
-        mOtherToggle.setClickable(false);
     }
 
     private void enableOtherToggles() {
-        if (mOtherToggle == null) {
-            return;
+        for (View button : mButtons) {
+            if (button != this)
+                ((LayoutToggleButton) button).enable();
         }
-
-        mOtherToggle.setFocusable(true);
-        mOtherToggle.setClickable(true);
     }
 
-    private void setGlobalChecked() {
-        View rootView = getRootView();
-        rootView.setTag(LAYOUT_CHECKED, true);
+    private void disable() {
+        Toast.makeText(getContext(), "disable LayoutToggleButton", Toast.LENGTH_LONG).show();
     }
 
-    private void setGlobalUnchecked() {
-        View rootView = getRootView();
-        rootView.setTag(LAYOUT_CHECKED, null);
+    private void enable() {
+        Toast.makeText(getContext(), "enable LayoutToggleButton", Toast.LENGTH_LONG).show();
     }
 
-    private boolean getGlobalChecked() {
-        View rootView = getRootView();
-        return rootView.getTag(LAYOUT_CHECKED) != null;
-    }
+    //private void disableOtherToggles() {
+    //    if (mOtherToggle == null) {
+    //        return;
+    //    }
+    //
+    //    mOtherToggle.setFocusable(false);
+    //    mOtherToggle.setClickable(false);
+    //}
+    //
+    //private void enableOtherToggles() {
+    //    if (mOtherToggle == null) {
+    //        return;
+    //    }
+    //
+    //    mOtherToggle.setFocusable(true);
+    //    mOtherToggle.setClickable(true);
+    //}
+
+    //private void setGlobalChecked() {
+    //    View rootView = getRootView();
+    //    rootView.setTag(LAYOUT_CHECKED, true);
+    //}
+    //
+    //private void setGlobalUnchecked() {
+    //    View rootView = getRootView();
+    //    rootView.setTag(LAYOUT_CHECKED, null);
+    //}
+    //
+    //private boolean getGlobalChecked() {
+    //    View rootView = getRootView();
+    //    return rootView.getTag(LAYOUT_CHECKED) != null;
+    //}
 }
