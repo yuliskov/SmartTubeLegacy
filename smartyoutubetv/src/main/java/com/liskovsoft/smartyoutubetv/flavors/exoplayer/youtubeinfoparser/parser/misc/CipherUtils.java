b557b35d0284c62e24ca828c878ae8ea51f7eb44
Yuriy Liskov <firsthash@gmail.com>
2018-07-24 22:24:07 +0300

    4k: fix black screen

--- smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/youtubeinfoparser/parser/misc/CipherUtils.java
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/youtubeinfoparser/parser/misc/CipherUtils.java
@@ -4,9 +4,15 @@ import java.io.InputStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * YouTube's decipher routine tools<br/>
+ * Routine can have any form<br/>
+ * Main file: <em>"tv-player.js"</em><br/>
+ * Searched marks: a.split("") ... a.join("")
+ */
 public class CipherUtils {
     private static final String decipherPattern =
-            "var\\ [A-Za-z]{2}\\=\\{.*\\n.*\\n.*\\nfunction\\ [A-Za-z]{2}\\(a\\)\\{.*\\;return\\ a\\.join\\(\\\"\\\"\\)\\}";
+            "var\\ [A-Za-z]{2}\\=\\{.*\\n.*\\n.*\\nfunction\\ [_$A-Za-z]{2}\\(a\\)\\{.*\\;return\\ a\\.join\\(\\\"\\\"\\)\\}";
 
     public static String extractDecipherCode(InputStream is) {
         Scanner scanner = new Scanner(is);
@@ -15,7 +21,7 @@ public class CipherUtils {
         if (jsCode == null) {
             return "";
         }
-        String cleaned = jsCode.replaceFirst("function\\ [A-Za-z]{2}", "function decipherSignature");
+        String cleaned = jsCode.replaceFirst("function\\ [_$A-Za-z]{2}", "function decipherSignature");
         return cleaned;
     }
 }
