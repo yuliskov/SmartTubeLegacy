0ea77a03537dbac2bbf93d85e578ad8e4b9dd914
Yuriy Liskov <firsthash@gmail.com>
2018-10-09 20:52:36 +0300

    MiTV4A 43 fix: bit refactor

--- smartyoutubetv/src/main/assets/addons/on_init/03_codec_fix/01_codec_fix_config.js
+++ smartyoutubetv/src/main/assets/addons/on_init/03_codec_fix/01_codec_fix_config.js
@@ -23,13 +23,13 @@ var CodecConfig_WebView = {
     'MiTV3S (pulpfiction)': 'mp4', // MiTV3S 55/60 (4pda)
     'MStar Android TV (aosp_ponkan32)': 'mp4', // ??? (gmail)
     'MiTV3S (missionimpossible)': 'mp4', // MiTV3 ???
-    // 'MiTV4A (matrix)': 'mp4', // amlogic: MiTV4A (4pda)
     'MiTV4 (missionimpossible)': 'mp4', // MiTV4 65 (4pda)
     'NG3128HD': 'webm, mp4', // (github > issues)
     'Amazon Jem (cm_jem)': '', // Kindle Fire HD 8.9 (enable all formats)
     't95m': '', // (gmail)
     '32T18': '', // FAMILY GENERATION T18 BY (gmail)
     'mt5882': '', // Generic Android on mt5882 (gmail)
+    // 'MiTV4A (matrix)': 'mp4', // amlogic: MiTV4A 43 (4pda)
     // 'MiTV2-40 (hancock)': 'webm', // entrapment: MiTV2 40" (gmail)
     // 'OzoneHD (m201)': 'webm', // 4pda
     // '3719C (Hi3719CV100)': 'webm', // SYTV > Disqus
@@ -38,7 +38,7 @@ var CodecConfig_WebView = {
     // 'NEO-U1': 'webm', // (github)
     // 'Hi3798MV100': 'webm', // Q1EN.2004 (gmail, china)
     // 'X92': 'webm', // (gmail)
-    '': 'webm' // other devices (entry must be last in the list)
+    '': 'webm' // other devices (entry should be the last in the list)
 };
 
 /**
@@ -54,11 +54,11 @@ var CodecConfig_XWalk = {
     'MiTV3S (pulpfiction)': 'mp4', // MiTV3S 55/60 (4pda)
     'MStar Android TV (aosp_ponkan32)': 'mp4', // ??? (gmail)
     'MiTV3S (missionimpossible)': 'mp4', // MiTV3 ???
-    // 'MiTV4A (matrix)': 'mp4', // amlogic: MiTV4A (4pda)
     'MiTV4 (missionimpossible)': 'mp4', // MiTV4 65 (4pda)
     'Amazon Jem (cm_jem)': '', // Kindle Fire HD 8.9 (enable all formats)
     't95m': '', // (gmail)
     '32T18': '', // KTU84M (gmail, thailand)
+    // 'MiTV4A (matrix)': 'mp4', // amlogic: MiTV4A 43 (4pda)
     // 'MiTV3S-43 (hancock)': 'webm', // MiTV3S 43 ???
     // 'MiTV2-40 (hancock)': 'webm', // entrapment: MiTV2 40" (gmail)
     // 'OzoneHD (m201)': 'webm', // 4pda
@@ -68,5 +68,5 @@ var CodecConfig_XWalk = {
     // 'Hi3798MV100': 'webm', // Q1EN.2004 (gmail, china)
     // 'NEO-U1': 'webm', // (github)
     // 'X92': 'webm', // (gmail)
-    '': 'webm' // other devices (entry must be last in the list)
+    '': 'webm' // other devices (entry should be the last in the list)
 };
