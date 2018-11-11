a61f1b90259e0e713b695052f9b4ac71578ff25d
Yuriy Liskov <firsthash@gmail.com>
2018-11-11 14:48:10 +0200

    4K: possible autoplay fix

--- smartyoutubetv/src/main/assets/core/exoplayer/08_track_end_button.js
+++ smartyoutubetv/src/main/assets/core/exoplayer/08_track_end_button.js
@@ -4,6 +4,7 @@ function TrackEndFakeButton(selector) {
     this.selector = selector;
     this.retryCount = 5;
     this.retryDelay = 500;
+    this.checkTimeoutMS = 2000;
 
     this.playerJumpToEnd = function() {
         console.log("TrackEndFakeButton: I'm about to start off!");
@@ -11,22 +12,39 @@ function TrackEndFakeButton(selector) {
 
         var player = Utils.$('video');
         if (player) {
+            player.pause(); // seems that pause helps somehow to force playback
             player.play();
             console.log("TrackEndFakeButton: before jumping to the end: current time: " + player.currentTime + ", duration: " + player.duration);
 
             if (isNaN(player.duration) && this.retryCount > 0) {
                 this.retryJumpToEnd();
             } else if (isNaN(player.duration)) {
-                console.log("TrackEndFakeButton: something wrong, do workaround: switch to the next track");
-                var btn = ExoButton.fromSelector(PlayerActivityMapping.BUTTON_NEXT);
-                btn.setChecked(true);
+                this.pressNextButton();
             } else {
                 player.currentTime = player.duration - 1; // seek to the end (minus one second!)
-                player.pause();
+                player.pause(); // seems that pause helps somehow to force playback
                 player.play();
+                this.startPlaybackCheck(player);
                 console.log("TrackEndFakeButton: after jumping to the end: current time: " + player.currentTime + ", duration: " + player.duration);
             }
         }
+    };
+
+    this.pressNextButton = function() {
+        console.log("TrackEndFakeButton: something wrong, do workaround: switch to the next track");
+        var btn = ExoButton.fromSelector(PlayerActivityMapping.BUTTON_NEXT);
+        btn.setChecked(true);
+    };
+
+    this.startPlaybackCheck = function(player) {
+        var position = player.currentTime;
+        var $this = this;
+        setTimeout(function() {
+            var positionNotChanged = position == player.currentTime;
+            if (positionNotChanged) {
+               $this.pressNextButton();
+           }
+        }, this.checkTimeoutMS);
     };
 
     this.retryJumpToEnd = function() {
