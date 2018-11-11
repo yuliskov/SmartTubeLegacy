df995a3a1ad8f5b27fb1d3feebf39e0fb4e2e7c4
Yuriy Liskov <firsthash@gmail.com>
2018-11-11 19:41:37 +0200

    utils: clear app cache

--- common/src/main/java/com/liskovsoft/smartyoutubetv/common/helpers/Helpers.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv/common/helpers/Helpers.java
@@ -14,6 +14,7 @@ import android.view.WindowManager;
 
 import java.io.ByteArrayInputStream;
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
@@ -306,5 +307,35 @@ public final class Helpers {
                num.contains(":")                                ||
                !Character.isDigit(num.charAt(num.length() - 1)) ||
                !Character.isDigit(num.charAt(0));
+    }
+
+    /**
+     * Deletes cache of app that belongs to the given context
+     * @param context app activity or context
+     */
+    public static void deleteCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (String child : children) {
+                boolean success = deleteDir(new File(dir, child));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
     }
 }
