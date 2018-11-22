59612476903c826c2d22df01c73193dbdb226793
Yuriy Liskov <firsthash@gmail.com>
2018-11-22 19:10:36 +0200

    exo: bg playback fix #6

--- smartyoutubetv/src/main/assets/core/exoplayer/08_track_end_button.js
+++ smartyoutubetv/src/main/assets/core/exoplayer/08_track_end_button.js
@@ -10,20 +10,26 @@ function TrackEndFakeButton(selector) {
         console.log("TrackEndFakeButton: I'm about to start off!");
         window.lastButtonName = PlayerActivity.TRACK_ENDED;
 
+        var $this = this;
         var player = Utils.$('video');
         if (player) {
-            // player.pause(); // seems that pause helps somehow to force playback
-            // player.play();
+            player.pause(); // seems that pause helps somehow to force playback
+            player.play();
             console.log("TrackEndFakeButton: before jumping to the end: current time: " + player.currentTime + ", duration: " + player.duration);
 
-            if (isNaN(player.duration) && this.retryCount > 0) {
-                this.retryJumpToEnd();
-            } else if (isNaN(player.duration)) {
+            if (this.retryCount <= 0) {
                 this.pressNextButton();
+                return;
+            }
+
+            this.retryCount--;
+
+            if (isNaN(player.duration)) {
+                setTimeout(function() { // do retry
+                    $this.playerJumpToEnd();
+                }, this.retryDelayMS);
             } else {
                 player.currentTime = player.duration - 1; // seek to the end (minus one second!)
-                // player.pause(); // seems that pause helps somehow to force playback
-                player.play();
                 this.startPlaybackCheck(player);
                 console.log("TrackEndFakeButton: after jumping to the end: current time: " + player.currentTime + ", duration: " + player.duration);
             }
@@ -42,17 +48,9 @@ function TrackEndFakeButton(selector) {
         setTimeout(function() {
             var positionNotChanged = position == player.currentTime;
             if (positionNotChanged) {
-               $this.pressNextButton();
+                $this.playerJumpToEnd();
            }
         }, this.checkTimeoutMS);
-    };
-
-    this.retryJumpToEnd = function() {
-        this.retryCount--;
-        var $this = this;
-        setTimeout(function() {
-            $this.playerJumpToEnd();
-        }, this.retryDelayMS);
     };
 
     this.getChecked = function() {
