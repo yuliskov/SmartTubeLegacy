55210f895be7009dd931cf92c7f3516e6bf1933c
Yuriy Liskov <firsthash@gmail.com>
2018-11-27 15:18:10 +0200

    1080: player bg fix (issue #200)

--- smartyoutubetv/src/main/assets/addons/on_load/07_player_background_fix/02_player_background_fix.js
+++ smartyoutubetv/src/main/assets/addons/on_load/07_player_background_fix/02_player_background_fix.js
@@ -4,69 +4,23 @@
  * Fix that by replacing it with the simple black screen.
  */
 
-console.log("Scripts::Running script player_background_fix.js");
+console.log("Scripts::Running script player_background_fix2.js");
 
 function PlayerBackgroundFixAddon() {
-    this.loaderHtml = '<div class="loader-container"></div>';
     this.run = function() {
-        if (DeviceUtils.isWebView()) {
-            this.hideShowPlayerBackground();
-        }
+        this.replacePlayerBackground();
     };
-
-    this.hideShowPlayerBackground = function() {
+    this.replacePlayerBackground = function() {
         var player = document.getElementsByTagName('video')[0];
         if (!player) {
             console.log("PlayerBackgroundFixAddon:: player not exist... waiting for the player...");
-            Utils.postDelayed(this.hideShowPlayerBackground, this, 500);
+            Utils.postDelayed(this.replacePlayerBackground, this, 500);
             return;
         }
 
-        if (player.callbackSet) {
-            return;
-        }
-
-        var container = Utils.$(YouTubeConstants.PLAYER_WRAPPER_SELECTOR);
-
-        var loader = Utils.appendHtml(container, this.loaderHtml);
-
-        function startPlayer(event) {
-            var howStarted = event == null ? "normally" : "from event";
-            player.play();
-            console.log("PlayerBackgroundFixAddon:: startPlayer() " + howStarted);
-        }
-
-        function showPlayer() {
-            loader.style['display'] = 'none';
-            console.log("PlayerBackgroundFixAddon:: showPlayer()");
-        }
-
-        function hidePlayer(event) {
-            loader.style['display'] = 'block';
-            var howStarted = event == null ? " normally" : " from event";
-            console.log("PlayerBackgroundFixAddon:: hidePlayer() " + howStarted);
-        }
-
-        // fix when clicked on the next button
-        function hideShowPlayer() {
-            if (!player.played.length) { // player not initialized
-                hidePlayer();
-            } else {
-                showPlayer();
-            }
-            console.log("PlayerBackgroundFixAddon:: hideShowPlayer()");
-        }
-
-        hideShowPlayer();
-        startPlayer(); // its very important (fix constant reload)
-
-        player.addEventListener('loadstart', startPlayer, false); // start loading
-        player.addEventListener('playing', showPlayer, false); // finished loading
-        player.addEventListener('abort', hidePlayer, false); // video closed
-        window.addEventListener('hashchange', hideShowPlayer, false); // detect open new video without closing current
-        player.callbackSet = true;
+        player.poster = "data:image/gif,AAAA"; // transparent image
     };
 }
 
-if (!DeviceUtils.isExo())
+if (DeviceUtils.isWebView() && !DeviceUtils.isExo())
     new PlayerBackgroundFixAddon().run();
