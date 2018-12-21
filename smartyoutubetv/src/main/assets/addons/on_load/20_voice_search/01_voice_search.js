e66aecee49f3482959641c941da0f4dac1a8885c
Yuriy Liskov <firsthash@gmail.com>
2018-12-21 18:40:15 +0200

    exo: add voice search: update 2

--- smartyoutubetv/src/main/assets/addons/on_load/20_voice_search/01_voice_search.js
+++ smartyoutubetv/src/main/assets/addons/on_load/20_voice_search/01_voice_search.js
@@ -8,7 +8,31 @@ console.log("Scripts::Running script voice_search.js");
 var VoiceSearch = {
     TAG: 'VoiceSearch',
     open: function(searchText) {
-        Log.d(this.TAG, "go to search page");
-        
+        Log.d(this.TAG, "going to the search page");
+
+        this.navigateToTheSearchPage();
+        this.typeSearchText(searchText);
+        this.commitChanges();
+    },
+
+    navigateToTheSearchPage: function() {
+        if (location.hash.indexOf('search') != -1) {
+            return;
+        }
+
+        location.hash = '/search?resume';
+    },
+
+    typeSearchText: function(searchText) {
+        var input = Utils.$('#text-input input');
+        if (!input) {
+            return;
+        }
+
+        input.value = searchText;
+    },
+
+    commitChanges: function() {
+        EventUtils.triggerEvent('#text-input input', 'input', DefaultKeys.ENTER);
     }
 };
