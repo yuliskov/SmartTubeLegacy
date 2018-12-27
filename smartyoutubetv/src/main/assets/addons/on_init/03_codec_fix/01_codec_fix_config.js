50b22d3bcec95e2695d70f368ad72ee63c763d6f
Yuriy Liskov <firsthash@gmail.com>
2018-12-27 17:03:41 +0200

    js: MiBOX3 Pro fix

--- smartyoutubetv/src/main/assets/addons/on_init/03_codec_fix/01_codec_fix_config.js
+++ smartyoutubetv/src/main/assets/addons/on_init/03_codec_fix/01_codec_fix_config.js
@@ -11,6 +11,7 @@ console.log("Scripts::Running script codec_fix_config.js");
  * Config for the WebView browser's engine (1th launcher)
  */
 var CodecConfig_WebView = {
+    'MiBOX3_PRO (once)': 'mp4', // mt8173 (disqus), fix hangs on video loads
     // 'amlogic': 'mp4', // be careful: not all amlogic devices have troubles playing mp4 (issue #162)
     'Hi3798CV100 (Hi3798CV100)': 'mp4', // github issue #176
     'X96mini (p281)': 'mp4', // amlogic: Telegram group
