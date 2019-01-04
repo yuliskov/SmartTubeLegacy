222ff780a328a863d2266c5f77e80a4dc0adcb0e
Yuriy Liskov <firsthash@gmail.com>
2019-01-05 01:48:48 +0200

    js: enable extrnal kbd: update 1

--- smartyoutubetv/src/main/assets/addons/on_load/09_enable_external_keyboard/01_enable_external_keyboard.js
+++ smartyoutubetv/src/main/assets/addons/on_load/09_enable_external_keyboard/01_enable_external_keyboard.js
@@ -7,24 +7,39 @@
 console.log("Scripts::Running script enable_external_keyboard.js");
 
 function EnableExternalKeyboardAddon() {
+    this.TAG = 'EnableExternalKeyboardAddon';
     this.run = function() {
-        this.enableExternalKeyboard();
+        this.enableExternalKeyboard2();
+    };
+
+    this.enableExternalKeyboard2 = function() {
+        var $this = this;
+
+        var handler = function(e) {
+            if (Utils.hasClass(e.target, YouTubeClasses.NO_MODEL)) {
+                return;
+            }
+
+            $this.enableSearchInputField();
+
+            EventUtils.removeListener(
+                YouTubeSelectors.SEARCH_PAGE,
+                YouTubeEvents.MODEL_CHANGED_EVENT,
+                handler);
+        };
+
+        EventUtils.addListener(
+            YouTubeSelectors.SEARCH_PAGE,
+            YouTubeEvents.MODEL_CHANGED_EVENT,
+            handler);
     };
 
     /**
-     * Imitate click on the input field
+     * Imitates click on the input field
      */
-    this.enableExternalKeyboard = function() {
-        var testFn = function() {
-            return Utils.$(YouTubeConstants.SEARCH_FIELD_SELECTOR);
-        };
-
-        var callback = function() {
-            var searchInput = Utils.$(YouTubeConstants.SEARCH_FIELD_SELECTOR);
-            EventUtils.triggerEvent(searchInput, DefaultEvents.KEY_UP, DefaultKeys.UP);
-        };
-
-        Utils.delayTillTestFnSuccess(callback, testFn);
+    this.enableSearchInputField = function() {
+        var searchInput = Utils.$(YouTubeConstants.SEARCH_FIELD_SELECTOR);
+        EventUtils.triggerEvent(searchInput, DefaultEvents.KEY_UP, DefaultKeys.UP);
     };
 }
 
