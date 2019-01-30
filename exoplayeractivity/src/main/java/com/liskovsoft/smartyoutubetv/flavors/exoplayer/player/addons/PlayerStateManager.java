2f810af1c3ace80e2c4a0c6b97f8d60cc80152da
Yuriy Liskov <firsthash@gmail.com>
2019-01-30 15:46:14 +0200

    exo: track selection fix

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/addons/PlayerStateManager.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/addons/PlayerStateManager.java
@@ -35,6 +35,7 @@ public class PlayerStateManager {
     private static final int RENDERER_INDEX_VIDEO = PlayerCoreFragment.RENDERER_INDEX_VIDEO;
     private static final int RENDERER_INDEX_SUBTITLE = PlayerCoreFragment.RENDERER_INDEX_SUBTITLE;
     private static final int HEIGHT_PRECISION_PX = 10; // ten-pixel precision
+    private static final float FPS_PRECISION = 10; // fps precision
     private static final long MIN_PERSIST_DURATION_MILLIS = 5 * 60 * 1000; // don't save if total duration < 5 min (most of songs)
     private static final long MAX_TRAIL_DURATION_MILLIS = 3 * 1000; // don't save if 3 sec of unseen video remains
     private static final long MAX_START_DURATION_MILLIS = 30 * 1000; // don't save if video just starts playing < 30 sec
@@ -194,7 +195,7 @@ public class PlayerStateManager {
 
                     boolean codecMatch = trackCodecs == null || codecEquals(format.codecs, trackCodecs);
                     boolean heightMatch = heightEquals(format.height, trackHeight) || format.height <= trackHeight;
-                    boolean fpsMatch = format.frameRate <= trackFps;
+                    boolean fpsMatch = fpsEquals(format.frameRate, trackFps);
 
                     if (codecMatch && heightMatch && fpsMatch) {
                         result.add(myFormat);
@@ -265,6 +266,10 @@ public class PlayerStateManager {
 
     private boolean heightEquals(int leftHeight, int rightHeight) {
         return Math.abs(leftHeight - rightHeight) <= HEIGHT_PRECISION_PX;
+    }
+
+    private boolean fpsEquals(float leftFps, float rightFps) {
+        return Math.abs(leftFps - rightFps) <= FPS_PRECISION;
     }
 
     private boolean tracksEquals(String leftTrackId, String rightTrackId) {
