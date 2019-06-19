8a3f23396a13891a5a5d8ce989674cf2c016bab4
Yuriy Liskov <firsthash@gmail.com>
2019-06-19 12:47:11 +0300

    exo: preiviews #5

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/widgets/previewtimebar/ExoPlayerManager.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/widgets/previewtimebar/ExoPlayerManager.java
@@ -4,20 +4,36 @@ import android.widget.ImageView;
 import com.bumptech.glide.request.target.Target;
 import com.github.rubensousa.previewseekbar.PreviewLoader;
 import com.google.android.exoplayer2.Player;
+import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv.flavors.exoplayer.youtubeinfoparser.parsers.YouTubeStoryParser.Size;
 import com.liskovsoft.smartyoutubetv.flavors.exoplayer.youtubeinfoparser.parsers.YouTubeStoryParser.Storyboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExoPlayerManager implements PreviewLoader {
+    private static final String TAG = ExoPlayerManager.class.getSimpleName();
     private PreviewTimeBar mPreviewTimeBar;
     private ImageView mImageView;
     private final Storyboard mStoryBoard;
+    private int mLastImgNum = -1;
+    private List<Integer> mLoadedImages;
 
     public ExoPlayerManager(PreviewTimeBar previewTimeBar,
                             ImageView imageView,
                             Storyboard storyboard) {
+        mLoadedImages = new ArrayList<>();
         mImageView = imageView;
         mPreviewTimeBar = previewTimeBar;
         mStoryBoard = storyboard;
+
+        if (mStoryBoard == null) {
+            mPreviewTimeBar.setPreviewEnabled(false);
+        } else {
+            mPreviewTimeBar.setPreviewEnabled(true);
+        }
+
+        preloadNextImage();
     }
 
     public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
@@ -43,5 +59,29 @@ public class ExoPlayerManager implements PreviewLoader {
                 .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
                 .transform(transformation)
                 .into(mImageView);
+
+        mLastImgNum = imgNum;
+        preloadNextImage();
+    }
+
+    private void preloadNextImage() {
+        if (mStoryBoard == null) {
+            return;
+        }
+
+        mLastImgNum++; // get next image
+
+        if (mLoadedImages.contains(mLastImgNum)) {
+            return;
+        }
+
+        Log.d(TAG, "Oops, image #" + mLastImgNum + " didn't cached yet");
+
+        mLoadedImages.add(mLastImgNum);
+
+        String link = mStoryBoard.getThumbSetLink(mLastImgNum);
+        GlideApp.with(mImageView)
+                .load(link)
+                .preload();
     }
 }
