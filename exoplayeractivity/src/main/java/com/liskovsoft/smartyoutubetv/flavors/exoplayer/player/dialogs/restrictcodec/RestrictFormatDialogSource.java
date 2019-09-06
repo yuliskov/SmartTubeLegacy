4b8da6707d839c02715bee5009b24cd0f3bd5f97
Yuriy Liskov <firsthash@gmail.com>
2019-09-06 21:28:48 +0300

    exo: codec dialog upd

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/dialogs/restrictcodec/RestrictFormatDialogSource.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/dialogs/restrictcodec/RestrictFormatDialogSource.java
@@ -29,11 +29,11 @@ public class RestrictFormatDialogSource implements SingleDialogSource {
         mItems.add(new RestrictFormatDialogItem("FHD    30fps    vp9", "1080|30|vp9", mPrefs));
         mItems.add(new RestrictFormatDialogItem("FHD    60fps    avc+vp9", "1080|60|", mPrefs));
         mItems.add(new RestrictFormatDialogItem("FHD    30fps    avc+vp9", "1080|30|", mPrefs));
-        mItems.add(new RestrictFormatDialogItem("2K     60fps    vp9    hdr", "1440|60|vp9|hdr", mPrefs));
+        mItems.add(new RestrictFormatDialogItem("2K     60fps    vp9+hdr", "1440|60|vp9|hdr", mPrefs));
         mItems.add(new RestrictFormatDialogItem("2K     60fps    vp9", "1440|60|vp9", mPrefs));
         mItems.add(new RestrictFormatDialogItem("2K     60fps    avc+vp9", "1440|60|", mPrefs));
         mItems.add(new RestrictFormatDialogItem("2K     30fps    vp9", "1440|30|vp9", mPrefs));
-        mItems.add(new RestrictFormatDialogItem("4K     60fps    vp9    hdr", "2160|60|vp9|hdr", mPrefs));
+        mItems.add(new RestrictFormatDialogItem("4K     60fps    vp9+hdr", "2160|60|vp9|hdr", mPrefs));
         mItems.add(new RestrictFormatDialogItem("4K     60fps    vp9", "2160|60|vp9", mPrefs));
         mItems.add(new RestrictFormatDialogItem("4K     60fps    avc+vp9", "2160|60|", mPrefs));
         mItems.add(new RestrictFormatDialogItem("4K     30fps    vp9", "2160|30|vp9", mPrefs));
