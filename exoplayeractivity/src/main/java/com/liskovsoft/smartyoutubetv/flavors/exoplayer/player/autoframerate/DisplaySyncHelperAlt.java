21bda41cc336fc6a5fdc6e05714f3cc0747b5d3f
Yuriy Liskov <firsthash@gmail.com>
2019-10-16 21:50:59 +0300

    bump to 6.17.174

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/autoframerate/DisplaySyncHelperAlt.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/autoframerate/DisplaySyncHelperAlt.java
@@ -15,26 +15,26 @@ class DisplaySyncHelperAlt extends DisplaySyncHelper {
         super(context);
     }
 
-    @Override
-    protected HashMap<Integer, int[]> getUHDRateMapping() {
-        return super.getRateMapping();
-    }
-
-    @Override
-    protected HashMap<Integer, int[]> getRateMapping() {
-        HashMap<Integer, int[]> relatedRates = new HashMap<>();
-        relatedRates.put(1500, new int[]{6000, 3000});
-        relatedRates.put(2397, new int[]{2397, 2400, 3000, 6000});
-        relatedRates.put(2400, new int[]{2400, 3000, 6000});
-        relatedRates.put(2497, new int[]{5000, 2500});
-        relatedRates.put(2500, new int[]{5000, 2500});
-        relatedRates.put(2997, new int[]{6000, 2997, 3000});
-        relatedRates.put(3000, new int[]{6000, 3000});
-        relatedRates.put(5000, new int[]{5000, 2500});
-        relatedRates.put(5994, new int[]{5994, 6000, 3000});
-        relatedRates.put(6000, new int[]{6000, 3000});
-        return relatedRates;
-    }
+    //@Override
+    //protected HashMap<Integer, int[]> getUHDRateMapping() {
+    //    return super.getRateMapping();
+    //}
+    //
+    //@Override
+    //protected HashMap<Integer, int[]> getRateMapping() {
+    //    HashMap<Integer, int[]> relatedRates = new HashMap<>();
+    //    relatedRates.put(1500, new int[]{6000, 3000});
+    //    relatedRates.put(2397, new int[]{2397, 2400, 3000, 6000});
+    //    relatedRates.put(2400, new int[]{2400, 3000, 6000});
+    //    relatedRates.put(2497, new int[]{5000, 2500});
+    //    relatedRates.put(2500, new int[]{5000, 2500});
+    //    relatedRates.put(2997, new int[]{6000, 2997, 3000});
+    //    relatedRates.put(3000, new int[]{6000, 3000});
+    //    relatedRates.put(5000, new int[]{5000, 2500});
+    //    relatedRates.put(5994, new int[]{5994, 6000, 3000});
+    //    relatedRates.put(6000, new int[]{6000, 3000});
+    //    return relatedRates;
+    //}
 
     @Override
     public boolean restoreOriginalState(Window window) {
