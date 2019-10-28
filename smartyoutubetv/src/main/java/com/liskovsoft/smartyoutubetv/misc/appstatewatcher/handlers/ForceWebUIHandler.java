00502fe607599f1f03274ff4e0d94547f9016c70
Yuriy Liskov <firsthash@gmail.com>
2019-10-29 00:08:22 +0200

    force old ui: upd

--- smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/misc/appstatewatcher/handlers/ForceWebUIHandler.java
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/misc/appstatewatcher/handlers/ForceWebUIHandler.java
@@ -5,8 +5,8 @@ import android.webkit.CookieManager;
 import com.liskovsoft.smartyoutubetv.misc.appstatewatcher.AppStateWatcherBase.StateHandler;
 
 public class ForceWebUIHandler extends StateHandler {
-    private static final String NEW_UI_COOKIE = "VISITOR_INFO1_LIVE=cp3UVuEA3l4; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2020 15:42:26 GMT; httponly";
-    private static final String OLD_UI_COOKIE = "VISITOR_INFO1_LIVE=ErVksiAQ6pg; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2020 15:42:26 GMT; httponly";
+    private static final String NEW_UI_COOKIE = "VISITOR_INFO1_LIVE=cp3UVuEA3l4; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2022 15:42:26 GMT; httponly";
+    private static final String OLD_UI_COOKIE = "VISITOR_INFO1_LIVE=ErVksiAQ6pg; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2022 15:42:26 GMT; httponly";
 
     public ForceWebUIHandler(Activity context) {
     }
