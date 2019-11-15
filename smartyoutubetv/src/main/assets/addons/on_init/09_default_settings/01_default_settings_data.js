9ba8796dd6a9fd63aeb1536d22fe0b2d10fa43ea
Yuriy Liskov <firsthash@gmail.com>
2019-11-16 00:22:20 +0200

    js: default settings fix

--- smartyoutubetv/src/main/assets/addons/on_init/09_default_settings/01_default_settings_data.js
+++ smartyoutubetv/src/main/assets/addons/on_init/09_default_settings/01_default_settings_data.js
@@ -6,7 +6,7 @@ console.log("Scripts::Running script default_settings_data.js");
 
 var DefaultSettingsData = {
     // sound off by default
-    'yt.leanback.default::sound-enabled': '{"data":false,"expiration":1573148412532,"creation":1542044412532}',
+    'yt.leanback.default::sound-enabled': '{"data":false,"expiration":1857405068193,"creation":1542044412532}',
     // disable Google spying (aka improve youtube setting)
     'yt.leanback::device-retention-permission': '{"data":{"enabled":false},"expiration":1857405068193,"creation":1542045068193}'
 };
