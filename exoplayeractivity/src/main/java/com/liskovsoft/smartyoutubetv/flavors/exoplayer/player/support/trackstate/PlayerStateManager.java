60707ddabe5bdacf72218e7b28f999b5be8f2cef
Yuriy Liskov <firsthash@gmail.com>
2019-12-28 10:09:44 +0200

    exo: restore position fix upd4

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/support/trackstate/PlayerStateManager.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/support/trackstate/PlayerStateManager.java
@@ -28,7 +28,7 @@ public class PlayerStateManager extends PlayerStateManagerBase {
     private static final int RENDERER_INDEX_VIDEO = PlayerCoreFragment.RENDERER_INDEX_VIDEO;
     private static final int RENDERER_INDEX_SUBTITLE = PlayerCoreFragment.RENDERER_INDEX_SUBTITLE;
     private static final long MIN_PERSIST_DURATION_MILLIS = 5 * 60 * 1000; // don't save if total duration < 5 min (most of songs)
-    private static final long MAX_TRAIL_DURATION_MILLIS = 3 * 1000; // don't save if 3 sec of unseen video remains
+    private static final long MAX_TRAIL_DURATION_MILLIS = 10 * 1000; // don't save if 10 sec of unseen video remains
     private static final long MAX_START_DURATION_MILLIS = 30 * 1000; // don't save if video just starts playing < 30 sec
     private static final long DECODER_INIT_TIME_MS = 1_000;
     private final ExoPlayerBaseFragment mPlayerFragment;
