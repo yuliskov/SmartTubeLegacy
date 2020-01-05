1776919ab32c6e54441f6ea7ada5417174b3df17
Yuriy Liskov <firsthash@gmail.com>
2020-01-05 14:52:06 +0200

    exo: remove okhttp addon

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
@@ -1,13 +1,13 @@
 package com.liskovsoft.smartyoutubetv.flavors.exoplayer.player.helpers;
 
 import android.content.Context;
-import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
+//import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
-import com.liskovsoft.sharedutils.okhttp.OkHttpHelpers;
+//import com.liskovsoft.sharedutils.okhttp.OkHttpHelpers;
 import com.liskovsoft.smartyoutubetv.misc.UserAgentManager;
 
 public class ApplicationUtil {
@@ -17,11 +17,11 @@ public class ApplicationUtil {
         return new DefaultDataSourceFactory(context, bandwidthMeter, buildHttpDataSourceFactory(context, bandwidthMeter));
     }
 
-    public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
-        return new OkHttpDataSourceFactory(OkHttpHelpers.getOkHttpClient(), USER_AGENT_MANAGER.getUA(), bandwidthMeter);
-    }
-
     //public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
-    //    return new DefaultHttpDataSourceFactory(USER_AGENT_MANAGER.getUA(), bandwidthMeter);
+    //    return new OkHttpDataSourceFactory(OkHttpHelpers.getOkHttpClient(), USER_AGENT_MANAGER.getUA(), bandwidthMeter);
     //}
+
+    public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
+        return new DefaultHttpDataSourceFactory(USER_AGENT_MANAGER.getUA(), bandwidthMeter);
+    }
 }
