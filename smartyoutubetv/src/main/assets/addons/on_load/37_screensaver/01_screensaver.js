e6fc4ed13118f6153575e4b1d1c39355679eea7c
Yuriy Liskov <firsthash@gmail.com>
2020-01-08 00:31:53 +0200

    lite: screensaver fix upd

--- smartyoutubetv/src/main/assets/addons/on_load/37_screensaver/01_screensaver.js
+++ smartyoutubetv/src/main/assets/addons/on_load/37_screensaver/01_screensaver.js
@@ -7,6 +7,7 @@ console.log("Scripts::Running script screensaver.js");
 
 function ScreensaverAddon() {
     this.TAG = 'ScreensaverAddon';
+    this.isBlocked = false;
 
     this.run = function() {
         this.setupListener();
@@ -18,20 +19,35 @@ function ScreensaverAddon() {
         var blockScreensaver = function() {
             Log.d($this.TAG, "Blocking screensaver...");
 
+            if ($this.isBlocked) {
+                return;
+            }
+
             DeviceUtils.sendMessage(DeviceUtils.MESSAGE_DISABLE_SCREENSAVER);
+
+            $this.isBlocked = true;
         };
 
         var unblockScreensaver = function() {
             Log.d($this.TAG, "Unblocking screensaver...");
 
+            if (!$this.isBlocked) {
+                return;
+            }
+
             DeviceUtils.sendMessage(DeviceUtils.MESSAGE_ENABLE_SCREENSAVER);
+
+            $this.isBlocked = false;
         };
 
-        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_PLAY, blockScreensaver);
+        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_PLAYING, blockScreensaver);
 
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_PAUSE, unblockScreensaver);
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ENDED, unblockScreensaver);
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ERROR, unblockScreensaver);
+        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ABORT, unblockScreensaver);
+        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_EMPTYED, unblockScreensaver);
+        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_SUSPEND, unblockScreensaver);
     };
 }
 
