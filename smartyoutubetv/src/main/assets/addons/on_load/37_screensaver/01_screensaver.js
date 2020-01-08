c96d2bcd04972742aa8b9ff6dea37f27f1949c77
Yuriy Liskov <firsthash@gmail.com>
2020-01-08 19:24:20 +0200

    lite: js: screensaver fix

--- smartyoutubetv/src/main/assets/addons/on_load/37_screensaver/01_screensaver.js
+++ smartyoutubetv/src/main/assets/addons/on_load/37_screensaver/01_screensaver.js
@@ -16,38 +16,26 @@ function ScreensaverAddon() {
     this.setupListener = function() {
         var $this = this;
 
-        var blockScreensaver = function() {
-            Log.d($this.TAG, "Blocking screensaver...");
-
-            if ($this.isBlocked) {
-                return;
-            }
+        var blockScreensaver = function(e) {
+            Log.d($this.TAG, "Blocking screensaver... " + e.type);
 
             DeviceUtils.sendMessage(DeviceUtils.MESSAGE_DISABLE_SCREENSAVER);
-
-            $this.isBlocked = true;
         };
 
-        var unblockScreensaver = function() {
-            Log.d($this.TAG, "Unblocking screensaver...");
-
-            if (!$this.isBlocked) {
-                return;
-            }
+        var unblockScreensaver = function(e) {
+            Log.d($this.TAG, "Unblocking screensaver... " + e.type);
 
             DeviceUtils.sendMessage(DeviceUtils.MESSAGE_ENABLE_SCREENSAVER);
-
-            $this.isBlocked = false;
         };
 
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_PLAYING, blockScreensaver);
 
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_PAUSE, unblockScreensaver);
-        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ENDED, unblockScreensaver);
-        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ERROR, unblockScreensaver);
-        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ABORT, unblockScreensaver);
+        // EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ENDED, unblockScreensaver);
+        // EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ERROR, unblockScreensaver);
+        // EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_ABORT, unblockScreensaver);
         EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_EMPTYED, unblockScreensaver);
-        EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_SUSPEND, unblockScreensaver);
+        // EventUtils.addListener(YouTubeSelectors.PLAYER_OBJ, DefaultEvents.PLAYER_SUSPEND, unblockScreensaver);
     };
 }
 
