354883ed1d8a2a3643e8957b0f9ac66c26806908
Yuriy Liskov <firsthash@gmail.com>
2020-01-13 03:01:23 +0200

    refactor

--- smartyoutubetv/src/main/assets/addons/on_init/19_enter_long_press_fix/01_enter_long_press_fix.js
+++ smartyoutubetv/src/main/assets/addons/on_init/19_enter_long_press_fix/01_enter_long_press_fix.js
@@ -7,6 +7,7 @@ console.log("Scripts::Running script enter_long_press_fix.js");
 
 function EnterLongPressFixAddon() {
     this.TAG = 'EnterLongPressFixAddon';
+    this.lastEvent = {name: '', nums: 0};
 
     this.run = function() {
         this.applyLongPressFix();
@@ -17,35 +18,47 @@ function EnterLongPressFixAddon() {
 
         // There is a handler that blocks others. We should run before it.
         // Note: running on capture phase
+        EventUtils.addListener(document, DefaultEvents.ON_CLICK, function(e) {
+            Log.d($this.TAG, "User clicked on " + e.keyCode);
+            // e.stopPropagation();
+        }, true);
+
+        // There is a handler that blocks others. We should run before it.
+        // Note: running on capture phase
         EventUtils.addListener(document, DefaultEvents.KEY_PRESS, function(e) {
             Log.d($this.TAG, "User pressed " + e.keyCode);
+            // e.stopPropagation();
         }, true);
 
         // There is a handler that blocks others. We should run before it.
         // Note: running on capture phase
         EventUtils.addListener(document, DefaultEvents.KEY_UP, function(e) {
             Log.d($this.TAG, "User pressed up " + e.keyCode);
-
-            if (e.keyCode == DefaultKeys.ENTER) {
-                $this.lastEvent = DefaultEvents.KEY_UP;
-            }
+            // e.stopPropagation();
+            //
+            // if (e.keyCode == DefaultKeys.ENTER) {
+            //     $this.lastEvent.name = DefaultEvents.KEY_UP;
+            //     $this.lastEvent.nums = 0;
+            // }
         }, true);
 
         // There is a handler that blocks others. We should run before it.
         // Note: running on capture phase
         EventUtils.addListener(document, DefaultEvents.KEY_DOWN, function(e) {
-            if ($this.lastEvent == DefaultEvents.KEY_DOWN && e.keyCode == DefaultKeys.ENTER) {
-                e.stopPropagation();
-                return;
-            }
-
-            if (e.keyCode == DefaultKeys.ENTER) {
-                $this.lastEvent = DefaultEvents.KEY_DOWN;
-            }
+            //e.stopPropagation();
+            // if ($this.lastEvent.name == DefaultEvents.KEY_DOWN && $this.lastEvent.nums >= 1 && e.keyCode == DefaultKeys.ENTER) {
+            //     e.stopPropagation();
+            //     return;
+            // }
+            //
+            // if (e.keyCode == DefaultKeys.ENTER) {
+            //     $this.lastEvent.name = DefaultEvents.KEY_DOWN;
+            //     $this.lastEvent.nums++;
+            // }
 
             Log.d($this.TAG, "User pressed down " + e.keyCode);
         }, true);
     };
 }
 
-new EnterLongPressFixAddon().run();
+// new EnterLongPressFixAddon().run();
