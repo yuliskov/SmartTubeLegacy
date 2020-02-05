014664fc1631b60f7d303130d3fdd814f5acb93b
Yuriy Liskov <firsthash@gmail.com>
2020-02-05 17:56:20 +0200

    exo: revert focus lost fix

--- smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/common/TwoFragmentsManagerActivity.java
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/flavors/common/TwoFragmentsManagerActivity.java
@@ -138,9 +138,6 @@ public abstract class TwoFragmentsManagerActivity extends FragmentManagerActivit
 
         // fragment already attached, so only reorder it
         wrapper.bringToFront();
-
-        // fix lost focus
-        findViewById(R.id.exo_container).requestFocus();
     }
 
     private static boolean containsChild(ViewGroup container, View view) {
