fc3081c7e046f6b1c50168e2f45f8c4c2f30c51d
Yuriy Liskov <firsthash@gmail.com>
2020-03-24 03:15:23 +0200

    refactor

--- smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/misc/appstatewatcher/handlers/ForceNewUIHandler.java
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/misc/appstatewatcher/handlers/ForceNewUIHandler.java
@@ -7,10 +7,8 @@ import com.liskovsoft.smartyoutubetv.misc.appstatewatcher.AppStateWatcherBase.St
 
 public class ForceNewUIHandler extends StateHandler {
     private static final String TAG = ForceNewUIHandler.class.getSimpleName();
-    private static final String NEW_UI_COOKIE3 = "VISITOR_INFO1_LIVE=VsVpAXmklX4; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
-    private static final String NEW_UI_COOKIE2 = "VISITOR_INFO1_LIVE=xcc12hbEjFM; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
-    private static final String NEW_UI_COOKIE1 = "VISITOR_INFO1_LIVE=cp3UVuEA3l4; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
-    //private static final String OLD_UI_COOKIE = "VISITOR_INFO1_LIVE=ErVksiAQ6pg; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
+    private static final String NEW_UI_COOKIE = "VISITOR_INFO1_LIVE=sw1lN1SY3ts; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
+    private static final String OLD_UI_COOKIE = "VISITOR_INFO1_LIVE=gZAMS9rWkqc; path=/; domain=.youtube.com; expires=Sat, 25-Apr-2025 15:42:26 GMT; httponly";
     private static final String RESET_UI_COOKIE = "VISITOR_INFO1_LIVE=; path=/; domain=.youtube.com; expires=Sat, 25-Apr-1971 15:42:26 GMT; httponly";
     private static final String COOKIE_URL = "https://www.youtube.com";
     private final Activity mContext;
@@ -20,7 +18,7 @@ public class ForceNewUIHandler extends StateHandler {
     }
 
     @Override
-    public void onInit() {
+    public void onUpdate() {
         SmartUtils.setSecureCookie(RESET_UI_COOKIE, COOKIE_URL, mContext);
     }
 }
