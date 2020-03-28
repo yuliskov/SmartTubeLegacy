471c4dcb720885ef449c3638490b4c6e89b343ca
Yuriy Liskov <firsthash@gmail.com>
2020-03-28 18:13:01 +0200

    bump to 6.17.451

--- smartyoutubetv/src/main/assets/addons/on_init/55_external_player_watcher/01_external_player_watcher.js
+++ smartyoutubetv/src/main/assets/addons/on_init/55_external_player_watcher/01_external_player_watcher.js
@@ -1,6 +1,6 @@
 /**
  * Description:
- * Try to prevent accidental exit from the video after "suggestions" or "save to favorites"
+ * Try to prevent accidental exit from the video when running "suggestions" or "save to favorites"
  */
 
 console.log("Scripts::Running script external_player_watcher.js");
@@ -19,6 +19,22 @@ function ExternalPlayerWatcherAddon() {
         // Note: running on capture phase
         EventUtils.addListener(document, DefaultEvents.KEY_UP, function(e) {
             Log.d($this.TAG, "User pressed up " + e.keyCode);
+
+            if (YouTubeUtils.isPlayerOpened() && YouTubeUtils.isExoPlayerOpen()) {
+                if (YouTubeUtils.isPlayerControlsClosed() && !YouTubeUtils.isOverlayOpened()) {
+                    if (e.keyCode == DefaultKeys.ESC) {
+                        Log.d($this.TAG, "Blocking BACK while user using suggestions or favorites...");
+
+                        e.stopPropagation();
+                    }
+                }
+            }
+        }, true);
+
+        // There is a handler that blocks others. We should run before it.
+        // Note: running on capture phase
+        EventUtils.addListener(document, DefaultEvents.KEY_DOWN, function(e) {
+            Log.d($this.TAG, "User pressed down " + e.keyCode);
 
             if (YouTubeUtils.isPlayerOpened() && YouTubeUtils.isExoPlayerOpen()) {
                 if (YouTubeUtils.isPlayerControlsClosed() && !YouTubeUtils.isOverlayOpened()) {
