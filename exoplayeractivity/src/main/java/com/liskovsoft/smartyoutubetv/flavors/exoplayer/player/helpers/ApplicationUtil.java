e7263e8c834e3ba2f5e4f77a5b0416dd609fb9df
Yuriy Liskov <firsthash@gmail.com>
2020-05-29 00:49:26 +0300

    exo: new headers

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
@@ -9,10 +9,13 @@ import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.liskovsoft.smartyoutubetv.misc.HeaderManager;
 import com.liskovsoft.smartyoutubetv.misc.UserAgentManager;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class ApplicationUtil {
     private static final UserAgentManager USER_AGENT_MANAGER = new UserAgentManager();
+    private static final List<String> EXO_HEADERS = Arrays.asList("Origin", "Referer", "User-Agent", "Accept-Language", "Accept", "X-Client-Data");
 
     public static DataSource.Factory buildDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
         return new DefaultDataSourceFactory(context, bandwidthMeter, buildHttpDataSourceFactory(context, bandwidthMeter));
@@ -24,12 +27,20 @@ public class ApplicationUtil {
 
     public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
         DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(USER_AGENT_MANAGER.getUA(), bandwidthMeter);
-        //addCommonHeaders(context, dataSourceFactory);
+        addCommonHeaders(context, dataSourceFactory);
         return dataSourceFactory;
     }
 
     private static void addCommonHeaders(Context context, DefaultHttpDataSourceFactory dataSourceFactory) {
         HeaderManager headerManager = new HeaderManager(context);
-        dataSourceFactory.getDefaultRequestProperties().set(headerManager.getHeaders());
+        HashMap<String, String> headers = headerManager.getHeaders();
+
+        // NOTE: "Accept-Encoding" should set to "identity" or not present
+
+        for (String header : headers.keySet()) {
+            if (EXO_HEADERS.contains(header)) {
+                dataSourceFactory.getDefaultRequestProperties().set(header, headers.get(header));
+            }
+        }
     }
 }
