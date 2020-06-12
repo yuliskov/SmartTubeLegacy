daac792c3823dfef36edc9c2d074a3c4afa4455b
Yuriy Liskov <firsthash@gmail.com>
2020-06-13 01:33:28 +0300

    bump to 6.17.668

--- smartyoutubetv/src/main/assets/addons/on_init/17_ajax_interceptor/04_video_info_interceptor.js
+++ smartyoutubetv/src/main/assets/addons/on_init/17_ajax_interceptor/04_video_info_interceptor.js
@@ -10,27 +10,27 @@ function VideoInfoInterceptor() {
     this.VIDEO_INFO_URL = '/get_video_info?';
     this.shouldIntercept = true;
 
-    this.modifyOpen = function(rawArgs) {
-        var url = rawArgs[1];
-
-        if (url && url.indexOf(this.VIDEO_INFO_URL) >= 0) {
-            switch (window.VideoStatsWatcherAddon.recentVideoType) {
-                case window.VideoStatsWatcherAddon.VIDEO_TYPE_DEFAULT:
-                    Log.d(this.TAG, "Fix age restrictions... " + url);
-                    // unlock age restricted videos but locks some streams (use carefully!!!)
-                    url = url.replace("&el=leanback", "");
-                    url = url.replace("&ps=leanback", "");
-                    break;
-                case window.VideoStatsWatcherAddon.VIDEO_TYPE_LIVE:
-                case window.VideoStatsWatcherAddon.VIDEO_TYPE_UPCOMING:
-                case window.VideoStatsWatcherAddon.VIDEO_TYPE_UNDEFINED:
-                    // stream unlocking should happening in ExoInterceptor.java
-                    // otherwise you'll get playback errors in youtube
-                    //url = url.replace("&c=TVHTML5", "&c=HTML5");
-                    break;
-            }
-
-            rawArgs[1] = url;
-        }
-    };
+    // this.modifyOpen = function(rawArgs) {
+    //     var url = rawArgs[1];
+    //
+    //     if (url && url.indexOf(this.VIDEO_INFO_URL) >= 0) {
+    //         switch (window.VideoStatsWatcherAddon.recentVideoType) {
+    //             case window.VideoStatsWatcherAddon.VIDEO_TYPE_DEFAULT:
+    //                 Log.d(this.TAG, "Fix age restrictions... " + url);
+    //                 // unlock age restricted videos but locks some streams (use carefully!!!)
+    //                 url = url.replace("&el=leanback", "");
+    //                 url = url.replace("&ps=leanback", "");
+    //                 break;
+    //             case window.VideoStatsWatcherAddon.VIDEO_TYPE_LIVE:
+    //             case window.VideoStatsWatcherAddon.VIDEO_TYPE_UPCOMING:
+    //             case window.VideoStatsWatcherAddon.VIDEO_TYPE_UNDEFINED:
+    //                 // stream unlocking should happening in ExoInterceptor.java
+    //                 // otherwise you'll get playback errors in youtube
+    //                 //url = url.replace("&c=TVHTML5", "&c=HTML5");
+    //                 break;
+    //         }
+    //
+    //         rawArgs[1] = url;
+    //     }
+    // };
 }
