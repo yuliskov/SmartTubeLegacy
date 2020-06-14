aa554527adb3cb5237c8bfeeb00a019f50e88893
Yuriy Liskov <firsthash@gmail.com>
2020-06-14 13:11:50 +0300

    bump to 6.17.674

--- exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
+++ exoplayeractivity/src/main/java/com/liskovsoft/smartyoutubetv/flavors/exoplayer/player/helpers/ApplicationUtil.java
@@ -5,6 +5,7 @@ import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.upstream.HttpDataSource.BaseFactory;
 import com.liskovsoft.sharedutils.okhttp.OkHttpHelpers;
@@ -23,18 +24,18 @@ public class ApplicationUtil {
         return new DefaultDataSourceFactory(context, bandwidthMeter, buildHttpDataSourceFactory(context, bandwidthMeter));
     }
 
-    public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
-        OkHttpDataSourceFactory dataSourceFactory = new OkHttpDataSourceFactory(OkHttpHelpers.getOkHttpClient(), USER_AGENT_MANAGER.getUA(),
-                bandwidthMeter);
-        addCommonHeaders(context, dataSourceFactory);
-        return dataSourceFactory;
-    }
-
     //public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
-    //    DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(USER_AGENT_MANAGER.getUA(), bandwidthMeter);
-    //    addCommonHeaders(context, dataSourceFactory); // cause troubles for some users
+    //    OkHttpDataSourceFactory dataSourceFactory = new OkHttpDataSourceFactory(OkHttpHelpers.getOkHttpClient(), USER_AGENT_MANAGER.getUA(),
+    //            bandwidthMeter);
+    //    addCommonHeaders(context, dataSourceFactory);
     //    return dataSourceFactory;
     //}
+
+    public static HttpDataSource.Factory buildHttpDataSourceFactory(Context context, DefaultBandwidthMeter bandwidthMeter) {
+        DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory(USER_AGENT_MANAGER.getUA(), bandwidthMeter);
+        addCommonHeaders(context, dataSourceFactory); // cause troubles for some users
+        return dataSourceFactory;
+    }
 
     private static void addCommonHeaders(Context context, BaseFactory dataSourceFactory) {
         HeaderManager headerManager = new HeaderManager(context);
